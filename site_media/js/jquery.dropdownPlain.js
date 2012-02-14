1a020f0d09618c1ad3ab0361f941bfaed04f326e
Matt Mansour <slackbabbath@gmail.com>
2012-02-13 16:26:03 -0800

    Git adding css to site

---
+++ site_media/js/jquery.dropdownPlain.js
@@ -0,0 +1,18 @@
+
+$(function(){
+
+    $("ul.tree-menu-level-0  li").hover(function(){
+    
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+    
+    }, function(){
+    
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+    
+    });
+    
+    $("ul.tree-menu-level-0  li ul li:has(ul)").find("a:first").append(" &raquo; ");
+
+});
